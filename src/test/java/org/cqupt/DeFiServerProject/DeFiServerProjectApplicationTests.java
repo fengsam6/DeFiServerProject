5227e5d1b95cc0c2468342b2cae4791f4fb6f7a1
ChanZhuo <601868556@qq.com>
2020-08-24 21:03:34 +0800

    first add 创建springboot框架

---
+++ src/test/java/org/cqupt/DeFiServerProject/DeFiServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cqupt.DeFiServerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeFiServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
